40e8fe1763973f7f5f84c52f593c4e88ccce43e8
Ebube <aaronebube123@gmail.com>
2022-07-13 23:33:12 +0100

    minor file changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,26 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import styled from "styled-components";
+import TransactionsPage from "./TransactionsPage";
+
 function App() {
-	return <div className="App">hello world</div>;
+	const client = new ApolloClient({
+		cache: new InMemoryCache(),
+		uri: "http://localhost:4000/graphql",
+	});
+
+	return (
+		<ApolloProvider client={client}>
+			<div className="App">
+				<StyledHeader>Sub For Me Transaction History</StyledHeader>
+				<TransactionsPage />
+			</div>
+		</ApolloProvider>
+	);
 }
 
 export default App;
+
+const StyledHeader = styled.h1`
+	text-align: center;
+	padding-top: 40px;
+`;
